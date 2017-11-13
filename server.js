e579e2be5390efbd7823116a1b7eb4ac937c5199
William Chislett <wchislett@sorensonmedia.com>
2017-11-13 12:50:02 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+   socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+http.listen(port, function(){
+  console.log('listening on' + port.toString());
+});
+/*
+
+http.createServer(function (request, response) {
+    console.log('New connection');
+    userCount++;
+
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello!\n');
+    response.write('We have had '+userCount+' visits!\n');
+    response.end();
+}).listen(port);
+
+console.log('Server started');
+*/
