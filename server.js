0f582f1032d289ed91a7067a818f61349ccd66cc
William Chislett <wchislett@sorensonmedia.com>
2017-11-14 11:41:41 +0000

    Moving from heroku repo to github.

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const express = require('express');
+const http = require('http')
+const path = require('path');
+const socketIo = require('socket.io');
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+const MAX_MESSAGE_HISTORY = 1000;
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+let history = [];
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.emit('history', history);
 
-   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+        
+        if (history.length > MAX_MESSAGE_HISTORY) {
+          history = []
+        }
+
+        history.push(msg);
+        console.log(history.toString());
+    });
 });
 
 const port = process.env.PORT || 5000;
 
-http.listen(port, function(){
-  console.log('listening on' + port.toString());
+server.listen(port, () => {
+    console.log('listening on: ' + port.toString());
 });
 /*
 
